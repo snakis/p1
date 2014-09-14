65d99e8025e06389f342f01d29991c98c0acb94e
Samantha Nakis <samantha.nakis@gmail.com>
2014-09-13 23:18:51 -0400

    added picture and links

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <! doctype html>
 <html>
 <head>
-	<title>Project 1</title>
-	<meta charset='utf-8'>
+	<title>Samantha Nakis CSCI E-15 Class Projects</title>
 </head>
 
 <body>
+	<h1>Samantha Nakis CSCI E-15 Class Projects</h1>
+	<img src="images/sam_picture.jpg" alt="Samantha Nakis">
+	<br>
+	<a href="http://p2.samanthanakis.me/">Project 2</a>
+	<br>
+	<a href="http://p3.samanthanakis.me/">Project 3</a>
+	<br>
+	<a href="http://p4.samanthanakis.me/">Project 4</a>
+	<br>
 	<?php
 	echo 'This is my project 1 placeholder ';
 	?>
