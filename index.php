9bdb37951ee5df7551263ea0a6f01e29671adb65
Samantha Nakis <samantha.nakis@gmail.com>
2014-09-14 23:53:32 -0400

    Added new layout and some of the questions

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 </head>
 
 <body>
-	<h1>Samantha Nakis CSCI E-15 Class Projects</h1>
-	<img src="images/sam_picture.jpg" alt="Samantha Nakis">
+	<div id="header" style="background-color:DarkSalmon;">
+	<h1 style="margin-bottom:0;text-align:center">Samantha Nakis CSCI E-15 Projects</h1>
+	</div>
+
+	<div id="links" style="background-color:BurlyWood;height:200pxwidth:110px;float:left">
+	<b>Project Links</b>
 	<br>
 	<a href="http://p2.samanthanakis.me/">Project 2</a>
 	<br>
@@ -14,9 +18,17 @@
 	<br>
 	<a href="http://p4.samanthanakis.me/">Project 4</a>
 	<br>
-	<?php
-	echo 'This is my project 1 placeholder ';
-	?>
+	</div>
+
+	<div id="aboutme" style="background-color:#AntiqueWhite;height:200px;width:400px;float:left;">
+	<img src="images/sam_picture.jpg" alt="Samantha Nakis">
+	<br>
+	<h3>Who am I and what do I do?</h3><br>
+	<p>I am a manager at a software company for a group of software developers, all of which are new college graduates. Most of my day to day activities are project management and managing interactions with our customers. However, from time to time I also like to get my hands dirty with code.</p><br>
+	<h3>Why am I taking this course?</h3><br>
+	<p>Most of my software experience is with low-level programming and C++. However, the software world is quicky moving toward web-based everything. Applications that were previously written in C++ and installed on your desktop are now running as a web-app in your browser. I am hoping to gain some practical skills that will enable me develop these applications on my own. I am also hoping to gain skills that allow me to more compitently work with web-based teams at work.</p><br>
+	<h3>What previous experience do you have with programming?</h3><br>
+	</div>
 
 </body>
 </html>
