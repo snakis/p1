627d613c3de6517e92f46c25a5b85caa5d8c053b
Samantha Nakis <samantha.nakis@gmail.com>
2014-09-15 00:26:09 -0400

    Added new layout and some of the questions

--- index.php
+++ index.php
@@ -26,8 +26,11 @@
 	<h3>Who am I and what do I do?</h3><br>
 	<p>I am a manager at a software company for a group of software developers, all of which are new college graduates. Most of my day to day activities are project management and managing interactions with our customers. However, from time to time I also like to get my hands dirty with code.</p><br>
 	<h3>Why am I taking this course?</h3><br>
-	<p>Most of my software experience is with low-level programming and C++. However, the software world is quicky moving toward web-based everything. Applications that were previously written in C++ and installed on your desktop are now running as a web-app in your browser. I am hoping to gain some practical skills that will enable me develop these applications on my own. I am also hoping to gain skills that allow me to more compitently work with web-based teams at work.</p><br>
-	<h3>What previous experience do you have with programming?</h3><br>
+	<p>The software world is quicky moving toward web-based everything. Applications that were previously written in C++ and installed on your desktop are now running as a web-app in your browser. I am hoping to gain some practical skills that will enable me develop these applications on my own. I am also hoping to gain skills that allow me to more compitently work with web-based teams at work.</p><br>
+	<h3>What previous experience do I have with programming?</h3><br>
+	<p>My educational background is in computer engineering and most of my technical work as been in C/C++ and low-level programming.</p><br>
+	<h3>What platform am I using?</h3>
+	<p>I am working on a Windows 8 machine.</p><br>
 	</div>
 
 </body>
